ba1b7cb42a1ce8e445b6182da7b3a1e3444ed24c
cthuluhoop123 <dowzhong@gmail.com>
2018-05-06 22:50:30 +1000

    More shit

--- bot.js
+++ bot.js
@@ -26,15 +26,20 @@ class tradingBot extends EventEmitter {
   }
 
   _cacheInventory() {
-    let self = this
     return new Promise((resolve, reject) => {
-      self.manager.getInventoryContents(440, 2, true, (err, inventory) => {
+      this.manager.getInventoryContents(440, 2, true, (err, inventory) => {
         if (err) return reject(err)
-        self._inventory = inventory
-        setTimeout(self._cacheInventory.bind(self), 1000*60*30)
-        self.emit('debug', 'Cached Inventory')
+        this._inventory = inventory
+        this._cacheInventoryTimeout = setTimeout(this._cacheInventory.bind(this), 1000 * 60 * 30)
+        this.emit('debug', 'Cached Inventory')
         return resolve(true)
       })
+    })
+  }
+
+  acceptOffer(offer) {
+    return new Promise((resolve, reject) => {
+      
     })
   }
 
@@ -42,37 +47,62 @@ class tradingBot extends EventEmitter {
     let receiving = offer.itemsToReceive.map(item => item.market_hash_name)
     let giving = offer.itemsToGive.map(item => item.market_hash_name)
     let receivingValue = receiving.reduce((accumulator, currentValue) => {
-      return accumulator + this.prices[currentValue].buy
+      if (this.prices[currentValue]) {
+        return accumulator + this.prices[currentValue].buy
+      } else {
+        return accumulator
+      }
     })
     let givingValue = giving.reduce((accumulator, currentValue) => {
-      return accumulator + this.prices[currentValue].sell
+      if (this.prices[currentValue]) {
+        return accumulator + this.prices[currentValue].sell
+      } else {
+        return accumulator + 9999
+      }
     })
     return Number(receivingValue - givingValue)
   }
 
   evaluateStock(offer) {
+    //falsy if an item is overstocked
+    let receiving = offer.itemsToReceive.map(item => item.market_hash_name)
     
+    for (let item of receiving) {
+      let amountOfItemOffered = receiving.reduce((accumulator, currentValue) => {
+        if (currentValue == item) {
+          return accumulator + 1
+        } else {
+          return accumulator
+        }
+      })
+      let amountOfItemInInventory = this._inventory.filter(item => item.market_hash_name == item).length
+      if (amountOfItemInInventory + amountOfItemOffered > this.prices[item].stock) {
+        return false
+      }
+    }
+    return true
   }
 
   logOn() {
-    let self = this
     return new Promise((resolve, reject) => {
-      self.client.logOn(self.logOnOptions)
-      self.client.on('webSession', (sessionID, cookies) => {
-        self.manager.setCookies(cookies, (err) => {
+      this.client.logOn(this.logOnOptions)
+      this.client.on('webSession', (sessionID, cookies) => {
+        this.community.setCookies(cookies)
+
+        this.community.on('sessionExpired', () => {
+          this.client.webLogOn()
+        })
+        
+        this.manager.setCookies(cookies, (err) => {
           if (err) {
             return reject(err)
           }
-          self._cacheInventory()
-          self.emit('debug', "Got API key: " + self.manager.apiKey)
+          this._cacheInventory()
+          this.emit('debug', 'Got API key: ' + this.manager.apiKey)
+          this.client.loggedOn = true
+          return resolve(true)
         })
-        self.community.setCookies(cookies)
       })
-      self.community.on('sessionExpired', () => {
-        self.client.webLogOn()
-      })
-      self.client.loggedOn = true
-      return resolve(true)
     })
   }
 
@@ -84,14 +114,18 @@ class tradingBot extends EventEmitter {
   }
 
   startTrading() {
-    let self = this
-    return new Promise((resolve, reject) => {
-      if (!self.client.loggedOn) return reject('Client must be logged on.')
-      self.manager.on('newOffer', async offer => {
-        self.evaluateOffer(offer)
-      })
-      return resolve(true)
+    if (!this.client.loggedOn) return reject('Client must be logged on.')
+    this.manager.on('newOffer', async offer => {
+      this.evaluateOffer(offer)
     })
+    this.emit('debug', 'Started Trading')
+  }
+
+  stopTrading() {
+    this.manager.removeListener('newOffer', async offer => {
+      this.evaluateOffer(offer)
+    })
+    this.emit('debug', 'Stopped Trading')
   }
 }
 
--- eventHandlers/clientEventHandler.js
+++ eventHandlers/clientEventHandler.js
@@ -1,17 +1,13 @@
+function forwardEvent(event, ...args) {
+  this.emit(event, ...args);
+}
+
 class clientEventHandler {
   constructor(tradingBot, steamClient) {
-    steamClient.on('loggedOn', (details, parental) => {
-      tradingBot.emit('loggedOn', details, parental)
-    })
-    steamClient.on('steamGuard', (domain, callback, lastCodeWrong) => {
-      tradingBot.emit('steamGuard', domain, callback, lastCodeWrong)
-    })
-    steamClient.on('error', error => {
-      tradingBot.emit('error', error)
-    })
-    steamClient.on('webSession', (sessionID, cookies) => {
-      tradingBot.emit('webSession', sessionID, cookies)
-    })
+    steamClient.on('loggedOn', ...args => forwardEvent.bind(tradingBot, 'loggedOn', ...args))
+    steamClient.on('steamGuard', ...args => forwardEvent.bind(tradingBot, 'steamGuard', ...args))
+    steamClient.on('error', ...args => forwardEvent.bind(tradingBot, 'error', ...args))
+    steamClient.on('webSession', ...args => forwardEvent.bind(tradingBot, 'webSession', ...args))
   }
 }
 
